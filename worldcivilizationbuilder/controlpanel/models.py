f4d21acce2b4dc7ec3d28341406a21f47d8a6b23
Ryan Orlando <rynorlando@gmail.com>
2021-07-16 12:28:48 -0400

    Add __str__ method to Civilization

--- worldcivilizationbuilder/controlpanel/models.py
+++ worldcivilizationbuilder/controlpanel/models.py
@@ -2,14 +2,17 @@ from django.db import models
 
 
 class Civilization(models.Model):
-	name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Tile(models.Model):
-	colum = models.IntegerField()
-	row = models.IntegerField()
-	controler = models.ForeignKey(
-		Civilization, 
-		null=True,
-		related_name="tiles",
-		on_delete=models.SET_NULL)
+    colum = models.IntegerField()
+    row = models.IntegerField()
+    controler = models.ForeignKey(
+        Civilization, 
+        null=True,
+        related_name="tiles",
+        on_delete=models.SET_NULL)
