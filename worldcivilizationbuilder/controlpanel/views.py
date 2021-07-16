fc6f627a0eee3dc0e925061c0ae9876550370943
Ryan Orlando <rynorlando@gmail.com>
2021-07-16 13:50:31 -0400

    Add bacis civiliztion list view.

--- worldcivilizationbuilder/controlpanel/views.py
+++ worldcivilizationbuilder/controlpanel/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 
 from django.http import HttpResponse
+from django.template import loader
 
+from controlpanel.models import Civilization
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    civilization_list = Civilization.objects.all()
+    template = loader.get_template('civilization_list.html')
+    context = {
+        'civilization_list': civilization_list,
+    }
+
+    return HttpResponse(template.render(context, request))
+
