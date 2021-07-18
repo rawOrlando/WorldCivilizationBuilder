81008ca4dae3be9c8085fa981e584ef6d4620e2b
Ryan Orlando <rynorlando@gmail.com>
2021-07-18 09:08:22 -0400

    Add a year field to civilization

--- worldcivilizationbuilder/controlpanel/models.py
+++ worldcivilizationbuilder/controlpanel/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Civilization(models.Model):
     name = models.CharField(max_length=100)
+    # .0 = Spring, .25 = Summer, .5 = Fall, .75 = Winter
+    last_year_updated = models.FloatField()
 
     def __str__(self):
         return self.name
